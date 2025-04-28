abe9a4346697e80303ba50967ee918c331abdbf9
mansipatel2609 <mansipatel2609@gmail.com>
2025-04-27 17:10:40 -0700

    added the touch two block

--- record_sim_episodes.py
+++ record_sim_episodes.py
@@ -39,6 +39,8 @@ def main(args):
         policy_cls = PickAndTransferPolicy
     elif task_name == 'sim_insertion_scripted':
         policy_cls = InsertionPolicy
+    elif task_name == 'sim_touch_two_blocks':          #  ← NEW
+        policy_cls = PickAndTransferPolicy  
     else:
         raise NotImplementedError
 
